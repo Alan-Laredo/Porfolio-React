a32b77ee899d731c5d6bac597439fad6f979a806
Alan Laredo <AlanLaredo@hotmail.com>
2019-04-05 23:30:53 -0600

    Remove unused imports

--- src/Components/ToolbarItem/ToolbarItem.js
+++ src/Components/ToolbarItem/ToolbarItem.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./ToolbarItem.css";
 
-import ToolbarLogo from "../ToolbarLogo/ToolbarLogo";
-
 class ToolbarItem extends React.Component {
   render() {
     return <div className="ToolbarItem">{this.props.children}</div>;
