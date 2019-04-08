b078cefc03ae6c76137797ace3ae0f666f413140
Alan Laredo <AlanLaredo@hotmail.com>
2019-04-07 21:51:45 -0500

    Updates

---
+++ precache-manifest.6b423133b8d63df2ad5db3d22375a402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc837d87dc84aab39301",
+    "url": "/project-react/static/css/main.3be1e6a8.chunk.css"
+  },
+  {
+    "revision": "bc837d87dc84aab39301",
+    "url": "/project-react/static/js/main.4c552f69.chunk.js"
+  },
+  {
+    "revision": "290f747e1d5709d91bf6",
+    "url": "/project-react/static/js/runtime~main.194ee2fc.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/project-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ded02c790d938bd3ebd019ee04a3676f",
+    "url": "/project-react/static/media/Cover.ded02c79.jpg"
+  },
+  {
+    "revision": "158dc1d3fb3614d37f259dd598655425",
+    "url": "/project-react/index.html"
+  }
+];
