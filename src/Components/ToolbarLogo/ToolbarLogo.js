43bf6fea21d222acc93a7aa5e9125dea39971ef2
Alan Laredo <AlanLaredo@hotmail.com>
2019-04-08 23:36:59 -0500

    Rebrand site

--- src/Components/ToolbarLogo/ToolbarLogo.js
+++ src/Components/ToolbarLogo/ToolbarLogo.js
@@ -5,7 +5,23 @@ class ToolbarLogo extends React.Component {
   render() {
     return (
       <div className="ToolbarLogo">
-        <b>AL</b>
+        <i className="fab fa-stylish">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 496 512"
+            role="img"
+            className="svg-inline--fa fa-github fa-w-16"
+            data-icon="github"
+            data-prefix="fab"
+            focusable="false"
+            aria-hidden="true"
+          >
+            <path
+              fill="currentColor"
+              d="M 39.296265,454.01835 232.22082,14.1965 456.70373,497.8035 213.13835,280.17633 265.30631,258.56765 393.58534,410.23177 232.22082,122.14287 Z"
+            />
+          </svg>
+        </i>
       </div>
     );
   }
